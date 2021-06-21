ac549ceb11116475b11d3462a70a9082126f3b82
keva damar galih <kevadamarg@gmail.com>
2021-06-21 19:24:01 +0700

    Initial Commit

---
+++ k-resto/k_restoApp.swift
@@ -0,0 +1,17 @@
+//
+//  k_restoApp.swift
+//  k-resto
+//
+//  Created by keva on 21/06/21.
+//
+
+import SwiftUI
+
+@main
+struct k_restoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
